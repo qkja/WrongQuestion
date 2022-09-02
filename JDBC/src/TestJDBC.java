6c3a3924570cc36779a9bed147dd9b62bf526f89
algorithmic-jacko <qkj1205@163.com>
2022-09-02 22:53:51 +0800

    JDBC编程

---
+++ JDBC/src/TestJDBC.java
@@ -0,0 +1,16 @@
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Qkj
+ * Date: 2022-09-02
+ * Time: 22:21
+ */
+public class TestJDBC {
+    public static void main(String[] args) {
+        DataSource dataSource = new MysqlDataSource();
+    }
+}
